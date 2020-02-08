32404d5a43d5cac1e42684f724cf48be5ac45231
Mignon Du Plessis <mignonduplessis@mignons-mbp.home>
2020-02-07 23:18:54 -1000

    adding

--- VacationPY/api_keys.py
+++ VacationPY/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2d31e31c3f8bbe8420cd51b044e7087f"
+weather_api_key = "YOUR KEY HERE!"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
--- WeatherPY/api_keys.py
+++ WeatherPY/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2d31e31c3f8bbe8420cd51b044e7087f"
+weather_api_key = "6d9dbe4cd81d71f9f131b9d6943de6fa"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
